94b95d2070a363a88abd21a4e9cc3b5ad2ad3217
Alexander Birukov <a_birukov@bk.ru>
2021-11-11 12:05:05 +0600

    (bjs21) update file

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,22 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    } if (max < arr[i]) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
+  
 
   return { min: min, max: max, avg: avg };
 }
