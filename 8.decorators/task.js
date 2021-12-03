6ec316fd6ae5b390c30eee065e65d755308b80f2
Alexander Birukov <a_birukov@bk.ru>
2021-12-03 14:42:50 +0600

    (bjs81) update file

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,27 @@
+// Задача 1
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...rest) {
+    let hash = rest.join(',');
+    let existResult = cache.filter(cacheRecord => cacheRecord.hash === hash);
+    if (existResult.length === 1) {
+        console.log('Из кэша: ' + existResult[0].value);
+        return 'Из кэша: ' + existResult[0].value;
+    } 
+    else {
+      let value = func.call(this, ...rest);
+      console.log('Вычисляем: ' + value);
+      if (cache.length < 5) {   
+        cache.push({hash, value});
+      } 
+      else {
+        cache.unshift({hash, value});
+        cache.pop();
+      } 
+      return 'Вычисляем: ' + value;
+    }
+  }
+  return wrapper;
 }
 
 
